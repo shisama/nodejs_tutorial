47eefe4660799209fdc0b1dcf209a118597e19ef
Masashi Hirano <cherrydog07@gmail.com>
2018-08-28 00:37:27 +0900

    response with json

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(`http method: ${req.method}\n`, req.headers);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!\n');
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify({
+      id: 12345,
+      message: 'Hello World!\n'
+    }));
     res.end();
   });
 
